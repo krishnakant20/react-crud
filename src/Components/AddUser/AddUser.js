c7abde75cf468f05af15e72b6ee87a41d847f1b9
krishnakant20 <89294839+krishnakant20@users.noreply.github.com>
2022-03-08 19:18:35 +0530

    get post

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, FormControl, FormGroup, Input, InputLabel, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { addUser } from '../../Service/api';
+import {useNavigate} from 'react-router-dom';
 
-
+const useStyle = makeStyles({
+    container:{
+        width:'80%',
+        margin:'5% 0 0 25px',
+        '&>*':{
+            marginTop:20
+        }
+    }
+})
+const initialValues={
+    name:'',
+    username:'',
+    email:'',
+    phone:''
+}
 const AddUser = () => {
-  return (
-    <div>AddUser</div>
-  )
+    const classes = useStyle();
+
+    const [user,setUser]=useState(initialValues);
+    const {name,username,email,phone}=user;
+    const historyNavigate= useNavigate();
+
+    const onValueChange =(e)=>{
+        console.log(e.target.value);
+        setUser({...user,[e.target.name]:e.target.value});
+        console.log(user);
+    }
+
+    const addUserDetails = async()=>{
+        await addUser(user);
+        historyNavigate('/all');
+    }
+    return (
+        <>
+            <FormGroup className={classes.container}>
+                <Typography>Create New User</Typography>
+                <FormControl>
+                    <InputLabel>Name</InputLabel>
+                    <Input onChange={(e)=>onValueChange(e)} name='name' value={name} />
+                </FormControl>
+                <FormControl>
+                    <InputLabel>Username</InputLabel>
+                    <Input onChange={(e)=>onValueChange(e)} name='username' value={username} />
+                </FormControl>
+                <FormControl>
+                    <InputLabel>Gmail</InputLabel>
+                    <Input onChange={(e)=>onValueChange(e)} name='email' value={email} />
+                </FormControl>
+                <FormControl>
+                    <InputLabel>Phone</InputLabel>
+                    <Input onChange={(e)=>onValueChange(e)} name='phone' value={phone} />
+                </FormControl>
+                <Button onClick={()=>addUserDetails()} variant='contained' color='primary'>Create User</Button>
+            </FormGroup>
+        </>
+    )
 }
 
 export default AddUser
--- src/Components/AllUsers/AllUsers.js
+++ src/Components/AllUsers/AllUsers.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import { Button, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import React, { useEffect, useState } from 'react'
+import { getUsers } from '../../Service/api'
+import { makeStyles } from '@mui/styles';
+
+const useStyle = makeStyles({
+    table: {
+        width: '80%',
+        margin: '50px 0 0 0'
+    },
+    thead: {
+        '&>*': {
+            background: '#90EE90'
+        }
+    }
+})
 
 const AllUsers = () => {
-  return (
-    <div>AllUsers</div>
-  )
+
+    const [users, setUsers] = useState([]);
+    const classes = useStyle();
+
+
+    useEffect(() => {
+        getAllUsers();
+
+
+    }, [])
+
+
+    const getAllUsers = async () => {
+        const responseGET = await getUsers();
+        console.log(responseGET.data);
+        setUsers(responseGET.data);
+    }
+
+
+    return (
+        <>
+            <div>AllUsers</div>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow className={classes.thead}>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Name:</TableCell>
+                        <TableCell>Username</TableCell>
+                        <TableCell>Gmail</TableCell>
+                        <TableCell>Phone:</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        users.map(user => (
+                            <TableRow>
+                                <TableCell>{user.id}</TableCell>
+                                <TableCell>{user.name}</TableCell>
+                                <TableCell>{user.username}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>{user.phone}</TableCell>
+                                <TableCell>
+                                    <Button variant='contained' color='primary' style={{marginRight:10}} >Edit</Button>
+                                    <Button variant='contained' color='secondary' style={{marginRight:10}} >Delete</Button>
+                                    </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </>
+    )
 }
 
 export default AllUsers
